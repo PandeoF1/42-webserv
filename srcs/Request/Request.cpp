8572667ee8cfac707128ba99ec2d94a53a76b004
root <root@pc-adonis.localdomain>
2022-06-09 11:42:17 +0200

    http check

--- srcs/Request/Request.cpp
+++ srcs/Request/Request.cpp
@@ -1,14 +1,14 @@
 #include "../../includes/webserv.hpp"
 
 Request::Request(char *request, Server &server) :  
-    _server(server),
-    _return_code(200)
+	_server(server),
+	_return_code(200)
 {
-    (void)_server;
-    std::string new_request = request;
-    _default_request = new_request;
-    std::cout << "New request from port: " << server.get_port() << std::endl;
-    parse();
+	(void)_server;
+	std::string new_request = request;
+	_default_request = new_request;
+	std::cout << "New request from port: " << server.get_port() << std::endl;
+	parse();
 }
 
 Request::~Request(void) {
@@ -17,48 +17,67 @@ Request::~Request(void) {
 
 void    Request::parse()
 {
-    std::istringstream  requestString(_default_request);
-    std::string         line;
-    int i = 0;
+	std::istringstream  requestString(_default_request);
+	std::string         line;
+	int i = 0;
 
-    // Parcours toutes les lignes pour parser la request
-    while (std::getline(requestString, line) && _return_code != 400)
-    {
-        if (i == 0)
-            set_method(line);
-        i++;
-    }
+	// Parcours toutes les lignes pour parser la request
+	while (std::getline(requestString, line) && _return_code != 400)
+	{
+		if (i == 0)
+			set_method(line);
+		i++;
+	}
 }
 
 void    Request::set_method(std::string line)
 {
-    //Surement a changer avec un check dans un vector pour toutes les methodes possible
-    if ((line.find("GET") == std::string::npos) != 0 &&
-        (line.find("POST") == std::string::npos) != 0 && 
-        (line.find("DELETE") == std::string::npos) != 0) 
-    {
-        std::cerr << "Method not found or not at good place" << std::endl;
-        _return_code = 400;
-        return ;
-    }
-    if (line.find_first_of(' ') == std::string::npos)
-    {
-        std::cerr << "No space after method" << std::endl;
-        _return_code = 400;
-        return ;
-    }
-    //MANQUE Verif l'espace apres la methode
-    _method = line.substr(0, line.find_first_of(' '));
+	//Surement a changer avec un check dans un vector pour toutes les methodes possible
+	if ((line.find("GET") == std::string::npos) != 0 &&
+		(line.find("POST") == std::string::npos) != 0 && 
+		(line.find("DELETE") == std::string::npos) != 0) 
+	{
+		std::cerr << "Method not found or not at good place" << std::endl;
+		_return_code = 400;
+		return ;
+	}
+	if (line.find_first_of(' ') == std::string::npos)
+	{
+		std::cerr << "No space after method" << std::endl;
+		_return_code = 400;
+		return ;
+	}
+	//MANQUE Verif l'espace apres la methode
+	_method = line.substr(0, line.find_first_of(' '));
 }
 
-// void    Request::check_http_version(std::string line) {
-//     int i = line.find_last_of(' ');
-// }
+void	Request::check_http_version(std::string line) {
+	int i = line.find("HTTP/1.1");
+	if (i == std::string::npos)
+	{
+		std::cerr << "HTTP version not found" << std::endl;
+		_return_code = 400;
+		return ;
+	}
+	if (line.find(" ", i - 1) != i - 1)
+	{
+		std::cerr << "No space before HTTP version" << std::endl;
+		_return_code = 400;
+		return ;
+	}
+	i = line.find("HTTP/1.1");
+	if (line.find_first_not_of("\n\r ", i + 8) != std::string::npos)
+	{
+		std::cerr << "Invalid char after HTTP version" << std::endl;
+		_return_code = 400;
+		return ;
+	}
+}
 
 std::string Request::get_method() const {
-    return (_method);
+	return (_method);
 }
 
 int Request::get_code() const {
-    return (_return_code);
+	return (_return_code);
 }
