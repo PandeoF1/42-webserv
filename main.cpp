487b7091f2e5795da307a95390f6c02d4c9ab76a
tnard <theo.nard18@gmail.com>
2022-06-23 16:51:22 +0200

    Edit: Config.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 15:54:51 by nard              #+#    #+#             */
-/*   Updated: 2022/06/23 11:22:40 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/06/23 16:50:36 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(void)
 {
 	std::map<int, Config> config = Config::createConfig("config.conf");
-	Config::returnPath(config[0], "/post_body/post_body/post_body/post_body");
+	Config::returnPath(config[0], "/test/");
 	//Config::print(config);
 	//start_servers();
  	return (0);
