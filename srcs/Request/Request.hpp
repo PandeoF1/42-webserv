04f8f55b46a7898cc265a420965133ee6025288d
Jeremy <jbosquet@student.42lyon.fr>
2022-07-13 12:32:31 +0200

    Clean request.cpp

--- srcs/Request/Request.hpp
+++ srcs/Request/Request.hpp
@@ -15,13 +15,15 @@ class Request
 		int                                 _return_code;
 		std::string                         _ip;
 
-		void    parse();
-		void    set_method(std::string line);
-		void    set_target_path(std::string line);
-		void    check_http_version(std::string line);
-		void    check_first_line_words(std::string line);
-		bool    check_target_path(std::string line);
-		bool    checkMaxBody();
+		void   		parse();
+		void    	set_method(std::string line);
+		void    	set_target_path(std::string line);
+		void    	check_http_version(std::string line);
+		void    	check_first_line_words(std::string line);
+		bool    	check_target_path(std::string line);
+		bool    	checkMaxBody();
+		void		checkAccept();
+		void		checkHost();
 		std::string checkMethod(std::string line);
 		std::string	parse_line(std::string line);
 
@@ -29,15 +31,14 @@ class Request
 		Request(std::string request, Server &server);
 		~Request(void);
 
-		std::map<std::string, int>			get_accepted_type();
-		std::map<std::string, int>			get_content_type_map();
+		std::map<std::string, int>			get_accepted_type() const;
+		std::map<std::string, std::string>  get_headers() const;
 		std::string							get_query_string() const;
 		std::string							get_target_path() const;
 		std::string							get_method() const;
-		std::map<std::string, std::string>  get_headers() const;
+		std::string							getIp(void) const;
 		int									get_code() const;
 		void								set_code(int code);
 		void								setIp(char *ip);
-		std::string							getIp(void) const;
 		void								set_target_path_force(std::string line);
 };
